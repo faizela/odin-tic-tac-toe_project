bd346599cf9e88026513c0ed1794a6fac5757125
Feisal Ahmed <feisala132@gmail.com>
2024-07-09 07:24:49 +0100

    Adding Gameboard, DisplayGameBoard, players functions

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const Gameboard = (function() {
+   const gameboard = []
+  return {gameboard}
+})()
+
+const players = (function(name) {
+return {name}
+})
+//const feisal = players('feisal')
+
+const DisplayGameBoard = (function() {
+ const gameDisplay = '|-----|-----|-----|\n|-----|-----|-----|\n|-----|-----|-----|'
+ return {gameDisplay}
+})()
+
+
+
+
+
