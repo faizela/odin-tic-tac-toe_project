9afffb1d6fa148684ee81b602405ffebc24ceb5c
Feisal Ahmed <feisala132@gmail.com>
2024-07-22 21:35:25 +0100

    Adding gameboard func

--- scripts.js
+++ scripts.js
@@ -1,45 +1,28 @@
 
-const Gameboard = function() {
-  const rows = 3
-  const columns = 4
-  const board = []
+const Gameboard = (function() {
+ const rows = 3
+ const columns = 3
 
-  for (let i = 0; i < rows; i++) {
-    board[i] = []
-    for (let j = 0; j < columns; j++) {
-      board[i].push(Cell())
-    }
+ for (let i = 0; i < rows; i++) {
+  board[i] = []
+  for (let j = 0; j < columns; j++){
+    board[i].push(Cell())
   }
 
-   // This will be the method of getting the entire board that our
-  // UI will eventually need to render it.
-  const getBoard = () => board;
-  
+ }
 
-  const dropToken = (column, player) => {
 
-  const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
- 
-  if (!availableCells.length) return;
+ const getBoard = () => board
 
-  const lowestRow = availableCells.length - 1;
-  board[lowestRow][column].addToken(player);
-  
+ return {getBoard}
 
-  const printBoard = () => {
-    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-    console.log(boardWithCellValues);
-  };
+}) ()
 
-  return { getBoard, dropToken, printBoard };
-}
 
-}
+// function Players (p1_name, p2_name) {
+//   const tokens = ['o', 'x']
+//  const players = [
+//   {} 
+//  ]
+// }
 
-function Cell () {
-  let value = 0
-
-  const addToken = (player) => {
-    
-  }
-}
